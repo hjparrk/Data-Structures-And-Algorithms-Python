f3a9a1410fbb6951b8d173b399cd19aad862481f
hjparrk <hyojoon2372552@naver.com>
2022-07-24 20:12:35 +0900

    create Node class

---
+++ Linked_List.py
@@ -0,0 +1,14 @@
+# Linked list in Python
+
+# Node class
+class Node:
+    
+    # Function to initilise the Node object
+    def __init__(self, data):
+        self.data = data # Data to store
+        self.next = None # A Pointer to the next node
+        
+
+
+         
+    
