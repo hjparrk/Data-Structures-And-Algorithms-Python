12cbe5cf60c86c383553b05a698cc399e424627b
hjparrk <hyojoon2372552@naver.com>
2022-07-25 10:48:20 +0900

    create LinkedList class and main function to execute the program

--- Linked_List.py
+++ Linked_List.py
@@ -3,12 +3,37 @@
 # Node class
 class Node:
     
-    # Function to initilise the Node object
+    # Function to initialise the Node object
     def __init__(self, data):
         self.data = data # Data to store
         self.next = None # A Pointer to the next node
-        
 
+# Linked list class
+class LinkedList: 
+    
+    # Function to initialise the LinkedList object
+    def __init__(self, data):
+        
+        init_node = Node(data) # create an initial node
+        self.length = 1 # length of the linked list
+        self.head = init_node # A pointer to the head node of the list
+        self.tail = init_node # A pointer to the tail node of the list
+
+    # Function to create new Node
+    def createNode(self, data):
+        new_node = Node(data)
+    
+
+
+# main function
+def main():
+    linked_list = LinkedList(1)
+    
+if __name__=="__main__":
+    main()
+    
+    
+    
+    
 
          
-    
