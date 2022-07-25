8892ef803b51e45fe220d64e8e407a5d798076bd
hjparrk <hyojoon2372552@naver.com>
2022-07-25 15:08:48 +0900

    implement pop function in the LinkedList class

--- Linked_List.py
+++ Linked_List.py
@@ -13,11 +13,10 @@ class LinkedList:
     
     # Function to initialise the LinkedList object
     # Takes O(1) time complexity
-    def __init__(self, data):
-        init_node = self.__createNode(data) # create an initial node
-        self.length = 1 # length of the linked list
-        self.head = init_node # A pointer to the head node of the list
-        self.tail = init_node # A pointer to the tail node of the list
+    def __init__(self):
+        self.length = 0 # length of the linked list
+        self.head = None # A pointer to the head node of the list
+        self.tail = None # A pointer to the tail node of the list
 
     # Private function to create new Node (can only be accessed and called in this LinkedList class)
      # Takes O(1) time complexity
@@ -27,43 +26,76 @@ class LinkedList:
     
     # Function to append a new node to the linked list
     # Takes O(1) time complexity as we know the tail node
-    # Having a pointer to the head node, takes O(n) to append a new node 
+    # Having a pointer only to the head node, takes O(n) to append a new node 
     # since it has to iterate all the node to reach the tail
     def append(self, data):
         new_node = self.__createNode(data)
-        self.tail.next = new_node
-        self.tail = new_node
+        if self.length == 0 or self.head is None or self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        
+        print("append " + str(self.tail.data))
     
+    # Function to pop the tail node from the linked list
+    # Takes O(1) time complexity
+    # Not knowing the tail node, it has to iterate all nodes to the tail one
+    # So, it takes O(n) time complexity
+    def pop(self):
+        if self.length == 0 or self.head is None or self.tail is None:
+            return None
+        temp = self.head
+        prev = self.head
+        while temp.next:
+            prev = temp
+            temp = temp.next
+        print("pop " + str(self.tail.data))
+        if self.length == 1 or prev is temp:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = prev
+            self.tail.next = None
+        self.length -= 1            
+        
+        
+        
+        
     # Function to print all node's data iteratively from the head node to the tail
     # Takes O(n) time complexity, iterating through the whole list
     def print_list(self):
-        # cursor node
-        cursor = self.head
+        # temporary pointer node (cursor)
+        temp = self.head
         print()
         # Iterate the whole list
-        while cursor is not None:
-            # if cursor.next exists, then print " -> "
-            if(cursor.next is not None):
-                print("Node " + str(cursor.data) ,end=" -> ")
-            # Otherwise, print nothing at the end as cursor.next is None
+        while temp is not None:
+            # if temp.next exists, then print " -> "
+            if(temp.next is not None):
+                print("Node " + str(temp.data) ,end=" -> ")
+            # Otherwise, print nothing at the end as temp.next is None
             else:
-                print("Node " + str(cursor.data) ,end="")
-            # forward the cursor to the next node
-            cursor = cursor.next
+                print("Node " + str(temp.data) ,end="")
+            # forward the temp to the next node
+            temp = temp.next
         print()
     
-    def pop(self):
-        return None
-        
 
 # main function
 def main():
     
-    linked_list = LinkedList(1)
+    linked_list = LinkedList()
+    linked_list.append(1)
     linked_list.append(2)
     linked_list.append(3)
     linked_list.append(4)
+    linked_list.pop()
+    linked_list.pop()
     linked_list.print_list()
+    
+    print(linked_list.length)
     
 if __name__=="__main__":
     
