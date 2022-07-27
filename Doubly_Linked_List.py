743e568cb93161d56a16383e1cb3d6637d43b429
hjparrk <hyojoon2372552@naver.com>
2022-07-27 15:34:15 +0900

    implement pop first function

--- Doubly_Linked_List.py
+++ Doubly_Linked_List.py
@@ -45,26 +45,45 @@ class DoublyLinkedList:
         
         if self.length == 0:
             return None
-        elif self.length == 1:
+        
+        current = self.tail
+        if self.length == 1:
             self.head = None
             self.tail = None
-            self.length -= 1
-            return self.head
         else:
-            current = self.tail
             self.tail = self.tail.prev
             self.tail.next = None
             current.prev = None
-            self.length -= 1
-            return current
+            
+        self.length -= 1
+        return current
+    
+    def pop_first(self):
 
+        if self.length == 0:
+            return None
+        
+        current = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            current.next = None
+            
+        self.length -= 1
+        return current 
+            
+        
 def main():
     
     doubly_linked_list = DoublyLinkedList()
     doubly_linked_list.append(1)
     doubly_linked_list.append(2)
     doubly_linked_list.pop()
-    doubly_linked_list.pop()
+    doubly_linked_list.pop_first()
     doubly_linked_list.print_list()
     
     print("\n{}".format(doubly_linked_list.length))
