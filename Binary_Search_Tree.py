9d29bcfa3e73ad34516755eacf99511ce7428f8a
hjparrk <hyojoon2372552@naver.com>
2022-07-28 01:14:40 +0900

    initial binary search tree program commit

---
+++ Binary_Search_Tree.py
@@ -0,0 +1,11 @@
+class Node:
+    
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+class Bst:
+    
+    def __init__(self):
+        self.root = None
